06a95c0901a86f6e5138295a5b320c415f5e9432
妙林 <miaolin1993@foxmail.com>
2023-06-01 00:37:19 +0800

    feat: 补充剩余搜索算法

--- 算法/utils.js
+++ 算法/utils.js
@@ -1,4 +1,4 @@
-const arr = [6, 7, 3, 1, 9, 12, 9, 78]
+const arr = [6, 7, 3, 1, 9, 12, 9, 8, 2, 5, 78]
 // 数组中两个元素交换位置
 function swap(list, a, b) {
   const temp = list[a]
@@ -7,3 +7,14 @@ function swap(list, a, b) {
 }
 
 const len = arr.length
+
+function quickSort(list) {
+  const lens = list.length
+  if (lens < 2) return list
+  const base = list[0] // 基准
+  // 跳过取出的基准值，取出所有比基准值大的和比基准值小的
+  const min = list.slice(1).filter(val => val <= base)
+  const max = list.slice(1).filter(val => val > base)
+
+  return quickSort(min).concat(base).concat(quickSort(max))
+}
